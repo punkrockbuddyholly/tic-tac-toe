94913141f09664d1237fcec855c9cbc019ebd820
Mike McCabe <mrmikemccabe@gmail.com>
2020-03-08 18:01:03 +0000

    Add onClick to Cell

--- src/Components/Cell/Cell.js
+++ src/Components/Cell/Cell.js
@@ -4,11 +4,10 @@ import './cell.css';
 function Cell(props) {
     const value = props.value;
     return (
-        <div className="cell">
+        <div className="cell" onClick={props.onClick}>
             { value }
         </div>
     );
 }
-
 
 export default Cell;
