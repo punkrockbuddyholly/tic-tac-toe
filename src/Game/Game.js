75297bb8d248ca4c3ccd625a09992dc8b570ed07
Mike McCabe <mrmikemccabe@gmail.com>
2020-03-08 18:03:47 +0000

    Add Board to Game and handle Cell clicks and reset

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Board from '../Components/Board';
+
+export const initialGrid = '_'.repeat(9).split('').map( () => null);
+export const players = ['×', '○'];
+export const intitialState = {
+  grid: [...initialGrid],
+  currentPlayer: players[0],
+  gameOver: false,
+  winner: null,
+};
 
 function Game() {
+    const [grid, setGrid] = useState(intitialState.grid);
+    const [currentPlayer, setCurrentPlayer] = useState(intitialState.currentPlayer);
+  
+    const handleBoardClick = (cell) => {
+      if(grid[cell] === null) {
+        // Map over the grid and update the clicked cell's value
+        const newGrid = grid.map( (x, i) => (i === cell ? currentPlayer : x));
+        setGrid(newGrid);
+        setCurrentPlayer(currentPlayer === players[0] ? players[1] : players[0]);
+      }
+    };
+
+    const reset = () => {
+      setGrid(intitialState.grid);
+      setCurrentPlayer(intitialState.currentPlayer);
+    };
+
     return (
         <div className="game">
-            Your game here
+            <Board grid={grid} onClick={handleBoardClick} />
+            <button className='btn btn--reset' onClick={reset}>Reset</button>
         </div>
     );
 }
--- src/Game/Game.test.js
+++ src/Game/Game.test.js
@@ -1,9 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './Game';
+import { shallow, mount } from 'enzyme';
+import Game, { players } from './Game';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<Game />);
+});
+
+it('populates a cell when clicked', () => {
+  const component = mount(<Game />);
+  // Confirm it starts as null
+  expect(component.find('Cell').first().prop('value')).toBeNull();
+  component.find('Cell').first().simulate('click');
+  // Confirm it has updated following click
+  expect(component.find('Cell').first().prop('value')).toBe(players[0]);
+});
+
+it('populates cells with alternate players when clicked', () => {
+  const component = mount(<Game />);
+  component.find('Cell').forEach( (node) => {
+    node.simulate('click');
+  });
+  component.find('Cell').forEach( (node, i) => {
+    const expectedPlayer = players[i % 2]
+    expect(node.prop('value')).toBe(expectedPlayer);
+  });
+});
+
+it('doesn\'t repopulate an already populated cell', () => {
+  const component = mount(<Game />);
+  component.find('Cell').at(0).simulate('click');
+  component.find('Cell').at(0).simulate('click');
+  expect(component.find('Cell').at(0).prop('value')).toBe(players[0]);
+});
+
+it('resets the grid when Reset is clicked', () => {
+  const component = mount(<Game />);
+  component.find('Cell').forEach( (node) => {
+    node.simulate('click');
+  });
+  component.find('Cell').forEach( (node, i) => {
+    const expectedPlayer = players[i % 2]
+    expect(node.prop('value')).toBe(expectedPlayer);
+  });
+  component.find('button').simulate('click');
+  component.find('Cell').forEach( (node) => {
+    expect(node.prop('value')).toBe(null);
+  });
 });
